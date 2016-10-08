dcf2b295ff8214e2ee79ef46a285110bafb92cc2
Silas Brumwell <silas13579@gmail.com>
2016-10-08 16:22:33 -0400

    Fixed merge, added room joints
    FIxed the merge so that our code works properly. Added cracks at the
    corner of the room.

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,6 +1,6 @@
 const Post = require('../models/Post');
-<<<<<<< HEAD
 const User = require('../models/User');
+const Style = require('../models/Style');
 
 const positions = [
   [0,2,-1.9],
@@ -12,9 +12,6 @@ const positions = [
   [1,2,1.9],
   [-1.9,4,1]
 ]
-=======
-const Style = require('../models/Style');
->>>>>>> 6acbe91d013c993d47c0ffdb240b1903b2b90ba4
 
 /**
  * GET /
@@ -37,67 +34,47 @@ exports.styleIndex = (req, res) => {
  * Success page.
  */
 exports.postUpload = (req, res, next) => {
-<<<<<<< HEAD
-
   var numPo
-
-  Post.find({id: req.user.id}).lean().exec((err, posts) => {
-    const l = posts.length
-    numPo = posts.length
-    console.log(numPo)
-
-
-
-    const post = new Post({
-      id: req.user.id,
-      fileName: req.file.filename,
-      coordinates: positions[numPo]
-    });
-
-    post.save((err) => {
-=======
-  console.log(req.body.type)
+  
   if (req.body.type == "Post")
   {
-    const post = new Post({
-      id: req.user.id,
-      fileName: req.file.filename,
-      coordinates: [1.9,4,0]
-    });
-
-    post.save((err) => {
-      if (err) {
+    Post.find({id: req.user.id}).lean().exec((err, posts) =>
+    {
+      numPo = posts.length
+      console.log(numPo)
+      const post = new Post(
+      {
+        id: req.user.id,
+        fileName: req.file.filename,
+        coordinates: positions[numPo]
+      });
+      post.save((err) => 
+      {
         req.flash('failure', { msg: 'File was not uploaded successfully.' });
         return next(err);
-      }
+      });
+      req.flash('success', { msg: 'File was uploaded successfully.' });
+      return res.redirect('/upload');
     });
-    req.flash('success', { msg: 'File was uploaded successfully.' });
-    return res.redirect('/upload');
   }
   else
   {
-    const style = new Style({
+    const style = new Style(
+    {
       id: req.user.id,
       fileName: req.file.filename,
       type: req.body.type
     });
 
-    style.save((err) => {
->>>>>>> 6acbe91d013c993d47c0ffdb240b1903b2b90ba4
-      if (err) {
+    style.save((err) => 
+    {
+      if (err) 
+      {
         req.flash('failure', { msg: 'File was not uploaded successfully.' });
         return next(err);
       }
     });
     req.flash('success', { msg: 'File was uploaded successfully.' });
     return res.redirect('/upload');
-<<<<<<< HEAD
-
-  });
-
-
- 
-=======
   }
->>>>>>> 6acbe91d013c993d47c0ffdb240b1903b2b90ba4
 };
