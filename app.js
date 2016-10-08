d339820dc118145d583374ba8e83be5b2638811a
Michael Olmsted <molmsted98@gmail.com>
2016-10-08 01:17:46 -0400

    Changed signup options. Began VR routes.

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
+const demoController = require('./controllers/demo');
 
 /**
  * API keys and Passport configuration.
@@ -131,6 +132,7 @@ app.post('/account/profile', passportConfig.isAuthenticated, userController.post
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
+app.get('/demo', demoController.index);
 
 /**
  * API examples routes.
---
+++ controllers/demo.js
@@ -0,0 +1,9 @@
+/**
+ * GET /
+ * Demo page.
+ */
+exports.index = (req, res) => {
+  res.render('vr/demo', {
+    title: 'Demo'
+  });
+};
