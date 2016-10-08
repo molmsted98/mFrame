79900021e88da453c1ee34c01399f4a252e522c8
Silas Brumwell <silas13579@gmail.com>
2016-10-08 06:45:35 -0400

    Loops through all pictures
    The pictureHanger script loops through all items in the picture div and
    hangs them at places according to their coordinates, given currently in
    a placeholder array.

---
+++ public/js/pictureHanger.js
@@ -0,0 +1,39 @@
+var pictureDiv = document.getElementById("pictures")
+var pictures = pictureDiv.getElementsByTagName("img")
+var pic, width, height, ratio, depthExtra
+//placeholder array for the current number of pictures, will be loaded later
+var items = [
+	[0,3,-1.9],
+	[1,2,1.9],
+	[-1,2,1.9],
+	[1,4,-1.9]
+]
+for (i = 0; i < pictures.length; i++)
+{
+	pic = pictures[i]
+	width = pic.width
+    height = pic.height
+    ratio = width/height
+    if (items[i][2] < 0)
+    {
+    	depthExtra = items[i][2]-.01
+    }
+    else
+    {
+    	depthExtra = items[i][2]+.01
+    }
+    if (height < width)
+    {
+      height = 1
+      width = ratio
+    }
+    else
+    {
+      width = 1
+      height = 1/ratio
+    }
+    document.write('<a-image src="#'+pic.id+'" position="'+items[i][0]+' '+items[i][1]+' '+items[i][2]+'" rotation="0 0 0" width="' + width + '" height="' + height + '"></a-image>')
+    width += .1
+    height += .1
+    document.write('<a-image color="black" position="'+items[i][0]+' '+items[i][1]+' '+depthExtra+'" rotation="0 0 0" width="' + width + '" height="' + height + '"></a-image>')
+}
