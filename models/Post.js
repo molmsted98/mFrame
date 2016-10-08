0393b1b6cf0eaeacb5149593437e2f1795b28253
Michael Olmsted <molmsted98@gmail.com>
2016-10-08 11:08:14 -0400

    Updated placeholder values

--- models/Post.js
+++ models/Post.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   id: String,
   fileName: String,
+  coordinates: [Number]
 }, { timestamps: true });
 
 const Post = mongoose.model('Post', postSchema);
