042504ffd08044713b337996ab9ddf1954cb7cc4
Administrator <molmsted98@gmail.com>
2016-10-09 07:54:41 -0400

    Added recent users to homepage

--- controllers/home.js
+++ controllers/home.js
@@ -7,15 +7,22 @@ exports.getUsers = (req, res) => {
   if (req.body.search)
   {
     User.find({username: new RegExp(req.body.search, "i")}).lean().exec((err, users) => {
-      res.render('home', {
-        title: 'Home',
-        "users": users
+      User.find().sort('-date').limit(5).exec(function(err, users2){
+        res.render('home', {
+          title: 'Home',
+          "users": users,
+          "users2": users2
+        });
       });
     });
   }
-  else {
-    res.render('home', {
-      title: 'Home'
+  else
+  {
+    User.find().sort('-date').limit(5).exec(function(err, users2){
+      res.render('home', {
+        title: 'Home',
+        "users2": users2
+      });
     });
   }
 };
